33deae740e3fde6f85c6aae6c8b064a6be23268a
zahodinikola <zahodinikola@gmail.com>
2017-06-07 06:57:28 +0300

    button bug

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -15,30 +15,41 @@ function loginController($scope) {
     $scope.authorisationErrorMessage = "";
     
     $scope.isValidEmail = function() {
-        if($scope.userInput.email === undefined) return; 
-        var x = $scope.userInput.email.toString();
-        var pattern = /[a-z0-9._]+@[a-z0-9._]+\.[a-z]{2,3}/;
-        if (pattern.test(x)) {
-            $scope.enteredValidEmail = "true";
-            $scope.emailFieldStyle = {"background-color": "lightgreen"};
-        }else{
-            $scope.enteredValidEmail = "false";
-            $scope.emailFieldStyle = {"background-color": "pink"};
-        }
+        if($scope.userInput.email !== undefined) {
+            var x = $scope.userInput.email.toString();
+            var pattern = /[a-z0-9._]+@[a-z0-9._]+\.[a-z]{2,3}/;
+            if (pattern.test(x)) {
+                $scope.enteredValidEmail = "true";
+                $scope.emailFieldStyle = {"background-color": "lightgreen"};
+            }else{
+                $scope.enteredValidEmail = "false";
+                $scope.emailFieldStyle = {"background-color": "pink"};
+            }
+            $scope.submitDisabled = $scope.isSubmitDisabled();
+            return;
+        };
+        $scope.enteredValidEmail = "false";
+        $scope.emailFieldStyle = {"background-color": "pink"};
         $scope.submitDisabled = $scope.isSubmitDisabled();
+    
     };
 
     $scope.isValidPassword = function() {
-        if($scope.userInput.password === undefined) return;
-        var x = $scope.userInput.password.toString();
-        var pattern = /[0-9a-zA-z]{6,}/;
-        if (pattern.test(x)) {
-            $scope.enteredValidPassword = "true";
-            $scope.passwordFieldStyle = {"background-color": "lightgreen"};
-        }else{
-            $scope.enteredValidPassword = "false";
-            $scope.passwordFieldStyle = {"background-color": "pink"};
-        }
+        if($scope.userInput.password !== undefined) {
+            var x = $scope.userInput.password.toString();
+            var pattern = /[0-9a-zA-z]{6,}/;
+            if (pattern.test(x)) {
+                $scope.enteredValidPassword = "true";
+                $scope.passwordFieldStyle = {"background-color": "lightgreen"};
+            }else{
+                $scope.enteredValidPassword = "false";
+                $scope.passwordFieldStyle = {"background-color": "pink"};
+            }
+            $scope.submitDisabled = $scope.isSubmitDisabled();
+            return;
+        };
+        $scope.enteredValidPassword = "false";
+        $scope.passwordFieldStyle = {"background-color": "pink"};
         $scope.submitDisabled = $scope.isSubmitDisabled();
     };
     
@@ -68,7 +79,8 @@ function loginController($scope) {
     };
 
     $scope.isSubmitDisabled = function() {
-        var submitDisabled = !(($scope.enteredValidEmail)&&($scope.enteredValidPassword));
-        return submitDisabled;
+        var _submitDisabled = ((!$scope.enteredValidEmail)||(!$scope.enteredValidPassword));
+        //alert("!(" + $scope.enteredValidEmail + '&&' + $scope.enteredValidPassword + ') = ' + _submitDisabled);
+        return _submitDisabled;
     };
 })
