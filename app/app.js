4377e7108aae86ce52df51dd11ee10d6a023021b
zahodinikola <zahodinikola@gmail.com>
2017-08-01 06:44:11 +0300

    initial routing made

--- app/app.js
+++ app/app.js
@@ -2,24 +2,60 @@
 
 var simpleForum = angular.module('simpleForum', ['ngRoute', 'ng-breadcrumbs']);
 
-// simpleForum.config(function($routeProvider, $locationProvider) {
-//         $locationProvider.hashPrefix('');
-//         $routeProvider.when('/login',
-//         {
-//             templateUrl: "./views/login.html",
-//             controller: "loginController",
-//             label: "Login"
-//         })
-//         .when('/signUp',
-//         {
-//             templateUrl: "./views/signUp.html",
-//             controller: "signupController",
-//             label: "Signup"
-//         })
-//         .when('/landing',
-//         {
-//             templateUrl: "./views/landing.html",
-//             controller: "landingController",
-//             label: "landing"
-//         })
-//     });
+simpleForum.config(function($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $routeProvider.when('/login',
+        {
+            templateUrl: "./views/login.html",
+            controller: "loginController",
+            label: "Login"
+        })
+        .when('/signUp',
+        {
+            templateUrl: "./views/signUp.html",
+            controller: "signupController",
+            label: "Signup"
+        })
+        .when('/editProfile',
+        {
+            templateUrl: "./views/editProfile.html",
+            controller: "editProfileController",
+            label: "EditProfile"
+        })
+        .when('/groups',
+        {
+            templateUrl: "./views/groups.html",
+            controller: "groupsController",
+            label: "Groups"
+        })
+        .when('/topics',
+        {
+            templateUrl: "./views/topics.html",
+            controller: "topicsController",
+            label: "Topics"
+        })
+        .when('/viewTopic',
+        {
+            templateUrl: "./views/viewTopic.html",
+            controller: "viewTopicController",
+            label: "View topic"
+        })
+        .when('/createTopic',
+        {
+            templateUrl: "./views/createTopic.html",
+            controller: "createTopicController",
+            label: "Create topic"
+        })
+        .when('/reply',
+        {
+            templateUrl: "./views/reply.html",
+            controller: "replyController",
+            label: "Reply"
+        })
+        .when('/replyQuoted',
+        {
+            templateUrl: "./views/replyQuoted.html",
+            controller: "replyQuotedController",
+            label: "Reply quoted"
+        });
+    });
